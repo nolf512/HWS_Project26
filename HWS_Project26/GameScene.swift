362364bd7e98af5e4aacac8dc71469a754a7d919
nofprog <k.jin3217@gmail.com>
2021-05-15 22:33:31 +0900

    isGameOver, didBegin

--- HWS_Project26/GameScene.swift
+++ HWS_Project26/GameScene.swift
@@ -16,7 +16,7 @@ enum CollisionTypes: UInt32 {
     case finish = 16
 }
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var scoreLabel: SKLabelNode!
     
@@ -32,6 +32,8 @@ class GameScene: SKScene {
     
     //モーション検出
     var motionManager: CMMotionManager!
+    
+    var isGameOver = false
   
     override func didMove(to view: SKView) {
      
@@ -48,7 +50,7 @@ class GameScene: SKScene {
         scoreLabel.zPosition = 2
         addChild(scoreLabel)
         
-        
+        physicsWorld.contactDelegate = self
         
         //デフォルトの重力をゼロにする
         physicsWorld.gravity = .zero
@@ -165,6 +167,8 @@ class GameScene: SKScene {
     }
     
     override func update(_ currentTime: TimeInterval) {
+        guard isGameOver == false else { return }
+        
         #if targetEnvironment(simulator)
         if let currentTouch = lastTouchPosition {
             let diff = CGPoint(x: currentTouch.x - player.position.x, y: currentTouch.y - player.position.y)
@@ -175,6 +179,41 @@ class GameScene: SKScene {
             physicsWorld.gravity = CGVector(dx: accelerometerData.acceleration.y * -50, dy: accelerometerData.acceleration.x * 50)
         }
         #endif
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        guard let nodeA = contact.bodyA.node else { return }
+        guard let nodeB = contact.bodyB.node else { return }
+        
+        if nodeA == player {
+            playerCollided(with: nodeB)
+        } else if nodeB == player {
+            playerCollided(with: nodeA)
+        }
+        
+    }
+    
+    func playerCollided(with node: SKNode){
+        if node.name == "vortex" {
+            player.physicsBody?.isDynamic = false
+            isGameOver = true
+            score -= 1
+            
+            let move = SKAction.move(to: node.position, duration: 0.25)
+            let scale = SKAction.scale(to: 0.0001, duration: 0.25)
+            let remove = SKAction.removeFromParent()
+            let sequence = SKAction.sequence([move, scale, remove])
+            
+            player.run(sequence) { [weak self] in
+                self?.createPlayer()
+                self?.isGameOver = false
+            }
+        } else if node.name == "star" {
+            node.removeFromParent()
+            score += 1
+        } else if node.name == "finish"{
+            
+        }
     }
     
 }
